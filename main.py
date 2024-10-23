b8cbe31752c56d6561944459000b1bc3bbe143a0
fonesst <rpfozzy@gmail.com>
2024-10-23 09:43:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,56 @@
 import telebot
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import logging
-import time
+import random
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# Токен бота
+TOKEN = '7368730334:AAH9xUG8G_Ro8mvV_fDQxd5ddkwjxHnBoeg'
+bot = telebot.TeleBot(TOKEN)
 
-# Замените на ваш API токен
-API_TOKEN = '7368730334:AAH9xUG8G_Ro8mvV_fDQxd5ddkwjxHnBoeg'
+# Полные списки имен
+male_names = [
+    "Авдей", "Авксентий", "Агапит", "Агафон", "Акакий", "Акиндин", "Александр", "Алексей", "Альберт", 
+    "Анатолий", "Андрей", "Антон", "Аркадий", "Арсений", "Артём", "Артур", "Богдан", "Борис", 
+    "Вадим", "Валентин", "Валерий", "Василий", "Виктор", "Владимир", "Владислав", "Вячеслав", 
+    "Гавриил", "Геннадий", "Георгий", "Герман", "Григорий", "Даниил", "Дмитрий", "Евгений", 
+    "Егор", "Иван", "Игорь", "Илья", "Кирилл", "Константин", "Максим", "Матвей", "Михаил", 
+    "Никита", "Николай", "Олег", "Павел", "Пётр", "Роман", "Руслан", "Сергей", "Степан", 
+    "Тимофей", "Фёдор", "Юрий"
+]
 
-bot = telebot.TeleBot(API_TOKEN)
+female_names = [
+    "Аврора", "Агафья", "Аглая", "Аделаида", "Александра", "Алина", "Алиса", "Алла", 
+    "Анастасия", "Анна", "Антонина", "Валентина", "Валерия", "Варвара", "Василиса", "Вера", 
+    "Вероника", "Галина", "Дарья", "Евгения", "Екатерина", "Елена", "Елизавета", "Зоя", 
+    "Ирина", "Кира", "Ксения", "Лариса", "Лидия", "Любовь", "Маргарита", "Марина", "Мария", 
+    "Надежда", "Наталья", "Ольга", "Полина", "Светлана", "София", "Татьяна", "Юлия", "Яна"
+]
 
-# Функция для запуска Selenium и работы с сайтом
-def process_site(user_text):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Открытие браузера в фоновом режиме
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
-    
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    
-    try:
-        driver.get('https://www.dencode.com/en/cipher/caesar')  # Сайт для шифрования
-        logger.info("Сайт загружен")
-        
-        # Ожидание и ввод текста в поле
-        wait = WebDriverWait(driver, 10)
-        input_field = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'textarea#v')))
-        input_field.clear()
-        input_field.send_keys(user_text)
-        logger.info(f"Введен текст: {user_text}")
-        
-        # Ожидание результата шифрования
-        result_field = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span#encCipherCaesar')))
-        result_text = result_field.text
-        logger.info(f"Результат шифрования: {result_text}")
-        
-        return result_text
+# Расширенный список фамилий
+surnames = [
+    "Иванов", "Петров", "Сидоров", "Федоров", "Васильев", "Кузнецов", "Новиков", "Смирнов", 
+    "Попов", "Киселев", "Зайцев", "Беляев", "Морозов", "Волков", "Соловьев", "Егоров", 
+    "Романов", "Александров", "Лебедев", "Григорьев", "Павлов", "Виноградов", "Богданов", 
+    "Крылов", "Дмитриев", "Гаврилов", "Михайлов", "Чернов", "Поляков", "Жуков", "Савельев", 
+    "Тарасов", "Борисов", "Фролов", "Карпов", "Шевченко", "Гончаров", "Мартынов", "Леонов"
+]
 
-    except Exception as e:
-        logger.error(f"Произошла ошибка: {str(e)}")
-        return None
+# Команда /genperson
+@bot.message_handler(commands=['genperson'])
+def send_gender_choice(message):
+    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup.add('Парень', 'Девушка')
+    bot.send_message(message.chat.id, "Выберите пол:", reply_markup=markup)
 
-    finally:
-        driver.quit()
-
-# Команда /start
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Введите текст, который нужно зашифровать.")
-
-# Обработка текста от пользователя
-@bot.message_handler(func=lambda message: True)
-def handle_text(message):
-    user_text = message.text
-    result = process_site(user_text)
-    
-    if result:
-        bot.reply_to(message, f"Результат шифрования: {result}")
+# Обработка выбора пола
+@bot.message_handler(func=lambda message: message.text in ['Парень', 'Девушка'])
+def generate_person(message):
+    if message.text == 'Парень':
+        name = random.choice(male_names)
     else:
-        bot.reply_to(message, "Произошла ошибка при обработке текста. Попробуйте снова.")
+        name = random.choice(female_names)
 
-bot.polling()
+    surname = random.choice(surnames)
+    bot.send_message(message.chat.id, f"Генерируемая личность: {name} {surname}")
+
+# Запуск бота
+print("Бот запущен...")
+bot.infinity_polling()
